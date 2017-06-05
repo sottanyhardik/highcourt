26b222cc98f96668dd01ad3b51551963e99c7508
hardik <h.sottany@gmail.com>
2017-06-05 08:27:49 +0530

    Settings

--- data/urls.py
+++ data/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,4 +8,5 @@ urlpatterns = [
     url(r'courts/$', views.CourtListView.as_view(), name='courts_list'),
     url(r'search/$', views.court_data, name='search'),
     url(r'^$', views.IndexView.as_view(), name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- highcourt/settings.py
+++ highcourt/settings.py
@@ -115,12 +115,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
 
-STATICFILES_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-MEDIA_URL = '/media/'
